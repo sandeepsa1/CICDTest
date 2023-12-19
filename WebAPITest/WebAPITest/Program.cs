d37dbc3dc9b4f5b5fd91f58a8b56983f87424619
git1213-test <154299705+git1213-test@users.noreply.github.com>
2023-12-19 18:15:39 +0530

    Update Program.cs

--- WebAPITest/WebAPITest/Program.cs
+++ WebAPITest/WebAPITest/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -10,16 +10,18 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
-app.MapControllers();
+//app.MapControllers();
+
+app.MapGet("time/utc", () => Results.Ok(DateTime.DaysInMonth));
 
 app.Run();
